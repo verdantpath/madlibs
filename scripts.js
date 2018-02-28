87971f613992613f8e5286b9f577e30072245dd5
verdantpath <m_dittmann@yahoo.com>
2018-02-27 22:48:46 -0800

    update js

--- scripts.js
+++ scripts.js
@@ -1,2 +1,4 @@
 var test = "Hello, world";
+
+var storyText = "I enjoy long, VAR walks on the beach, getting VAR in the rain and serendipitous encounters with VAR. I really like piña coladas mixed with VAR, and romantic, candle-lit VAR. I am well-read from Dr. Seuss to VAR. I travel frequently, especially to VAR, when I am not busy with work. (I am a VAR.)  I am looking for VAR and beauty in the form of a VAR goddess. She should have the physique of VAR and the VAR of VAR. I would prefer if she knew how to cook, clean, and wash my VAR. I know I’m not very attractive in my picture, but it was taken VAR days ago, and I have since become more VAR.";
 document.write(test);
